5758e799cdf9efcae353fb95d57155ee166e1566
Bernd van den Hoek <bernd_hoek@hotmail.com>
2016-10-03 13:38:05 +0200

    Correct startvalue

--- Mandelbrot/MandelNumber.cs
+++ Mandelbrot/MandelNumber.cs
@@ -13,7 +13,7 @@ namespace Mandelbrot
             return CalculateNumber(x, y);
         }
 
-        private int CalculateNumber(double a, double b, int mandelnumber = 0)
+        private int CalculateNumber(double a, double b, int mandelnumber = 1)
         {
             if (DistanceToOrigin(a, b) >= 2 || mandelnumber >= 100)
                 return mandelnumber;
