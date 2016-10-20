f9f4b84da8557833c3a1f5587bbdaf6a89ecd891
Bernd van den Hoek <bernd_hoek@hotmail.com>
2016-10-20 20:15:40 +0200

    add coordinates

---
+++ Mandelbrot/Coordinates.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Mandelbrot
+{
+     class Coordinates
+    {
+        private UserInterface userInterface;
+        private double leftBound, bottomBound, size;
+
+        public Coordinates(double centerX, double centerY, double scale, UserInterface userInterface)
+        {
+            leftBound = centerX - scale;
+            bottomBound = centerY - scale;
+            size = 2 * scale;
+            this.userInterface = userInterface;
+        }
+
+        /*
+         * x is the pixel of which you want to know the coordinate (relative to the panel)
+         * size * x / panelWidth is the offset
+         * leftBound + offset = x coordinate
+        */
+        public double GetXLocation(double x)
+        {
+            double offset = size * x / userInterface.GetPanelWidth();
+            return leftBound + offset;
+        }
+
+        /*
+         * y is the pixel of which you want to know the coordinate (relative to the panel)
+         * size * y / panelHeight is the offset
+         * bottomBount + offset = y coordinate
+        */
+        public double GetYLocation(double y)
+        {
+            double offset = size * y / userInterface.GetPanelHeight();
+            return bottomBound + offset;
+        }
+    }
+}
